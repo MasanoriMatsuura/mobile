7c81ac5d8cba950c00d3dd3f0a7e5a28a166771f
Matsu <62133380+MasanoriMatsuura@users.noreply.github.com>
2023-02-07 14:48:07 +0900

    Add files via upload

---
+++ bengla_mobile.R
@@ -0,0 +1,7 @@
+#Internet use and mobile phone ownership
+#author: Masanori Matsuura
+#2023/02/07
+
+#Import data
+ownership <- read_csv("~/Research/mobile_money/mobile_data/API_IT.CEL.SETS.P2_DS2_en_csv_v2_4772619/API_IT.CEL.SETS.P2_DS2_en_csv_v2_4772619.csv", skip = 3)
+internet <- read_csv("~/Research/mobile_money/mobile_data/API_IT.NET.USER.ZS_DS2_en_csv_v2_4770719/API_IT.NET.USER.ZS_DS2_en_csv_v2_4770719.csv", skip = 3)
